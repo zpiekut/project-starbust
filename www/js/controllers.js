f13941e5f86fa38600c2955e1af9cc797a996737
Amanda <amandalee.pgh@gmail.com>
2017-03-22 19:48:35 -0400

    added some partners page css to main css

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -57,6 +57,7 @@ function ($scope, $stateParams, RedemptionsService) {
 
 }])
 
+
 .controller('redeemJamesStPassCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
